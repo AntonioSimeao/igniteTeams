5fa0ab0b0fdde725528cce388eb3b3d5c1cf78ea
Antoniossn <antonionetosimeao@hotmail.com>
2023-03-04 15:53:41 -0300

    loading

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StatusBar } from "react-native";
+import {  StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import {
   useFonts,
@@ -7,6 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme/index";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +16,7 @@ export default function App() {
     // Se as fontes carregarem, vai mostrar o Groups, se não, vai mostrar um loading
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" />
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
