c400d5e8926e367770c93b5480db749219d6ce39
Antoniossn <antonionetosimeao@hotmail.com>
2023-03-04 15:07:03 -0300

    changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
-
+import { ActivityIndicator, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
+import theme from "./src/theme/index";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    // Se as fontes carregarem, vai mostrar o Groups, se não, vai mostrar um loading
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+  );
 }
