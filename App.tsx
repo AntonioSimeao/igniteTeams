54364b6951e2f40abd82cf237f6f37a25f2f3ce4
Antoniossn <antonionetosimeao@hotmail.com>
2023-03-08 13:59:10 -0300

    Players

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 import theme from "./src/theme/index";
 import { Loading } from "@components/Loading";
 
@@ -13,10 +13,10 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    // Se as fontes carregarem, vai mostrar o NewGroup, se não, vai mostrar um loading
+    // Se as fontes carregarem, vai mostrar o Players, se não, vai mostrar um loading
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" translucent />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
