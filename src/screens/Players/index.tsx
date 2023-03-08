12555de3774bdd0e101e7ef7634d7a99d277c5ef
Antoniossn <antonionetosimeao@hotmail.com>
2023-03-08 15:07:06 -0300

    Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,6 +2,8 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+
 import { Container, Form } from "./styles";
 
 export function Players() {
@@ -16,6 +18,7 @@ export function Players() {
         <Input placeholder="Nome da Pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+      <Filter title="Time A" />
     </Container>
   );
 }
