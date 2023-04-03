4c37b79a5b3e17ea5fabe1a877fa820ff26759c3
Antoniossn <antonionetosimeao@hotmail.com>
2023-04-03 17:43:12 -0300

    att

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,7 @@
+import { Alert, FlatList } from "react-native";
+import { useState } from "react";
+import { useRoute } from "@react-navigation/native";
+
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -6,35 +10,60 @@ import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 
 type RouteParams = {
-  group: string
-}
-
-import { FlatList } from "react-native";
-
-import { useState } from "react";
-import { useRoute } from "@react-navigation/native";
+  group: string;
+};
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState("");
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
 
-  const route = useRoute()
-  const {group} = route.params as RouteParams;;
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        'Nova pessoa',
+        'Informe o nome da pessoa para adicionar.'
+      );
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+      console.log(players);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Nova Pessoa", error.message);
+      } else {
+        console.log(error);
+        Alert.alert("Nova pessoa", "Não foi possivel adicionar");
+      }
+    }
+  }
 
   return (
     <Container>
       <Header showBackButton />
-      <Highlight
-        title={group}
-        subtitle="Adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="Adicione a galera e separe os times" />
       <Form>
-        <Input placeholder="Nome da Pessoa" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          onChangeText={setNewPlayerName}
+          placeholder="Nome da Pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
       <HeaderList>
         <FlatList
